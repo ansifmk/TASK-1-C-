591f1d9aacc65ff7e12e6f793b82eedebc6f308c
ansifmk <mohammedansifmka@gmail.com>
2025-11-14 10:38:20 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        int num1 = 20;
+        int num2 = 10;
+
+        int addition = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        double division = (double)num1 / num2;
+
+        Console.WriteLine("Addition: " + addition);
+        Console.WriteLine("Subtraction: " + subtraction);
+        Console.WriteLine("Multiplication: " + multiplication);
+        Console.WriteLine("Division: " + division);
+    }
+
+    public static void Main(string[] args)   
+    {
+        Class1 obj = new Class1();
+    }
+}
