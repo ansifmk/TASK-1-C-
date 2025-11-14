1972370c35e2c42c83e8f20bfcacf6ad64ee55f1
ansifmk <mohammedansifmka@gmail.com>
2025-11-14 12:06:40 +0530

    second day task 1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,24 +2,161 @@
 
 public class Class1
 {
-    public Class1()
-    {
-        int num1 = 20;
-        int num2 = 10;
+    //task 1
 
-        int addition = num1 + num2;
-        int subtraction = num1 - num2;
-        int multiplication = num1 * num2;
-        double division = (double)num1 / num2;
+    //public Class1()
+    //{
+    //    int num1 = 20;
+    //    int num2 = 10;
 
-        Console.WriteLine("Addition: " + addition);
-        Console.WriteLine("Subtraction: " + subtraction);
-        Console.WriteLine("Multiplication: " + multiplication);
-        Console.WriteLine("Division: " + division);
-    }
+    //    int addition = num1 + num2;
+    //    int subtraction = num1 - num2;
+    //    int multiplication = num1 * num2;
+    //    double division = (double)num1 / num2;
 
-    public static void Main(string[] args)   
-    {
-        Class1 obj = new Class1();
-    }
+    //    Console.WriteLine("Addition: " + addition);
+    //    Console.WriteLine("Subtraction: " + subtraction);
+    //    Console.WriteLine("Multiplication: " + multiplication);
+    //    Console.WriteLine("Division: " + division);
+    //}
+
+    //public static void Main(string[] args)   
+    //{
+    //    Class1 obj = new Class1();
+    //}
+
+
+
+
+
+
+    //task 2 -1qst
+
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("enter a number: ");
+    //    int number = Convert.ToInt32(Console.ReadLine());
+
+    //    if (number % 2 == 0)
+    //    { 
+    //    Console.WriteLine("The number is even.");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("The number is odd.");
+    //    }
+    //}
+
+
+
+    //task 3 -2qst
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("enter a number");
+    //    int number = Convert.ToInt32(Console.ReadLine());
+
+    //    if (number > 0) { 
+    //    Console.WriteLine("The number is positive.");
+    //    }
+    //    else if (number < 0)
+    //    {
+    //        Console.WriteLine("The number is negative.");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("The number is zero.");
+    //    }
+
+    //}
+
+
+    //Task 4 -3qst
+
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("enter a number: ");
+    //    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    //    Console.WriteLine("enter second number: ");
+    //    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    //    Console.WriteLine("enter third number: ");
+    //    int num3 = Convert.ToInt32(Console.ReadLine());
+
+    //    if (num1 >= num2 && num1 >= num3)
+    //    {
+    //        Console.WriteLine(num1 + " is the largest number . ");
+    //    }
+    //    else if (num2 >= num1 && num2 >= num3)
+    //    {
+    //        Console.WriteLine(num2 + " is the largest number . ");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine(num3 + " is the largest number . ");
+    //        {
+
+    //        }
+    //    }
+    //}
+
+
+
+    //task 5 -4qst
+
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("enter a year: ");
+    //    int year = Convert.ToInt32(Console.ReadLine());
+    //    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    //    {
+    //        Console.WriteLine(year + " is a leap year.");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine(year + " is not a leap year.");
+    //    }
+    //}
+
+
+    //task 6 -5qst
+
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("enter a mark: ");
+    //    int mark=Convert.ToInt32(Console.ReadLine());
+
+    //    if(mark >=90 && mark <=100)
+    //    {
+    //        Console.WriteLine("grade s");
+    //    }
+    //    else if(mark >=80 && mark <90)
+    //    {
+    //        Console.WriteLine("grade a");
+    //    }
+    //    else if(mark >=70 && mark <80)
+    //    {
+    //        Console.WriteLine("grade b");
+    //    }
+    //    else if(mark >=60 && mark <70)
+    //    {
+    //        Console.WriteLine("grade c");
+    //    }
+    //    else if(mark >=50 && mark <60)
+    //    {
+    //        Console.WriteLine("grade d");
+    //    }
+    //    else if(mark >=40 && mark <50)
+    //    {
+    //        Console.WriteLine("grade e");
+    //    }
+    //    else if(mark >=0 && mark <40)
+    //    {
+    //        Console.WriteLine("grade f");
+    //    }
+    //    else
+    //    {
+            
+    //    }
+    //}
 }
