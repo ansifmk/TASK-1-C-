952b4cdf735ac59d1c9b58ca81c8be42f8987981
ansifmk <mohammedansifmka@gmail.com>
2025-11-14 16:22:03 +0530

    new

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -278,34 +278,34 @@ public class Class1
 
     // task day 2 prime number
 
-    static void Main(string[] args)
-    {
-        {
-            Console.Write("Enter a number: ");
-            int num = int.Parse(Console.ReadLine());
+    //static void Main(string[] args)
+    //{
+    //    {
+    //        Console.Write("Enter a number: ");
+    //        int num = int.Parse(Console.ReadLine());
 
-            bool isPrime = true;
+    //        bool isPrime = true;
 
-            if (num <= 1)
-            {
-                isPrime = false;
-            }
-            else
-            {
-                for (int i = 2; i <= num / 2; i++)
-                {
-                    if (num % i == 0)
-                    {
-                        isPrime = false;
-                        break;
-                    }
-                }
-            }
+    //        if (num <= 1)
+    //        {
+    //            isPrime = false;
+    //        }
+    //        else
+    //        {
+    //            for (int i = 2; i <= num / 2; i++)
+    //            {
+    //                if (num % i == 0)
+    //                {
+    //                    isPrime = false;
+    //                    break;
+    //                }
+    //            }
+    //        }
 
-            if (isPrime)
-                Console.WriteLine(num + " is a Prime Number");
-            else
-                Console.WriteLine(num + " is NOT a Prime Number");
-        }
-    }
+    //        if (isPrime)
+    //            Console.WriteLine(num + " is a Prime Number");
+    //        else
+    //            Console.WriteLine(num + " is NOT a Prime Number");
+    //    }
+    //}
 }
